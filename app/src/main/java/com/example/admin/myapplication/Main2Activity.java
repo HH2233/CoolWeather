709e5334c386526c16d99913df67c73eb427fdaf
admin <944740635@qq.com>
2019-04-19 08:13:43 +0800

    第N次 无视提交

--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -1,25 +1,28 @@
 package com.example.admin.myapplication;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.widget.ArrayAdapter;
-        import android.widget.ListView;
-        import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-        import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-        import okhttp3.Call;
-        import okhttp3.Callback;
-        import okhttp3.Response;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private String[] data={"北京","浙江","安徽"};
+    private String[] data={"北京","浙江","安徽"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "};
     private TextView textView;
     private ListView listview;
-
-
-
 
 
     @Override
@@ -30,11 +33,16 @@ public class Main2Activity extends AppCompatActivity {
         this.listview=(ListView)findViewById(R.id.listview);
         ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
         listview.setAdapter(adapter);
+        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
 
 //        String weatherId="CN101020200";
         String weatherUrl = "http://guolin.tech/api/china";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
-
             @Override
             public void onFailure(Call call, IOException e) {
 
@@ -43,6 +51,8 @@ public class Main2Activity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
+                String[] result=parseJSONObject(responseText);
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -51,5 +61,22 @@ public class Main2Activity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    private String[] parseJSONObject(String responseText) {
+        JSONArray jsonArray= null;
+        try {
+            jsonArray = new JSONArray(responseText);
+            String[] result=new String[jsonArray.length()];
+            for (int i=0;i<jsonArray.length();i++){
+                JSONObject jsonObject=jsonArray.getJSONObject(i);
+                result[i]=jsonObject.getString("name");
+                this.data[i]=jsonObject.getString("name");
+            }
+            return result;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
